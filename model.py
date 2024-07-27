dcf73293bedef9b04916bd151fe5b1f29def1ffd
USER <hello@gsdadg.asdsadasd>
2024-07-27 12:30:39 +0300

    Чистый каркас

--- model.py
+++ model.py
@@ -1,14 +1,10 @@
 import pygame
 
-bullet=pygame.rect.Rect([500,-50,6,8])
-brick=pygame.rect.Rect([0,500,32,32])
 show_rects=False
-collide=False
+
 def model():
-    global collide
-    bullet.bottom+=3
-    if brick.colliderect(bullet):
-        collide=True
+    pass
+
 
 
 
--- view.py
+++ view.py
@@ -9,11 +9,4 @@ bullet=pygame.transform.scale(bullet,[6,8])
 def view():
     global screen
     screen.fill([0, 0, 0])
-    for line in range(0,1000,32):
-        screen.blit(brick,[line,500])
-        if model.show_rects:
-            pygame.draw.rect(screen, [255, 0, 0], [model.brick.x+line,model.brick.y,model.brick.w,model.brick.h], width=1)
-    screen.blit(bullet, [model.bullet.left, model.bullet.top])
-    if model.show_rects:
-        pygame.draw.rect(screen,[255,0,0],model.bullet,width=1)
     pygame.display.flip()
