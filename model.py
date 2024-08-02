789cc7918ee16bd8a3cf4606dc8dc01a95aec966
USER <hello@gsdadg.asdsadasd>
2024-08-02 16:29:50 +0300

    Чистый каркас 2

--- model.py
+++ model.py
@@ -1,6 +1,19 @@
+import random
+
 import pygame
 
 show_rects=False
+
+rects=[]
+
+blocks=10
+for line in range(blocks):
+    size=random.randint(10,100)
+    brick=pygame.rect.Rect([random.randint(10,100),random.randint(10,100),size])
+    rects.append(brick)
+
+def click_check():
+    pass
 
 def model():
     pass
