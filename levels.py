bbffaf6d15505cde21c57d3b2b93dfce034e4962
USER <hello@gsdadg.asdsadasd>
2025-02-05 19:04:38 +0300

    Чистый каркас 7

--- levels.py
+++ levels.py
@@ -30,7 +30,8 @@ level2={'map':"""00122100
         't_angle':270,
         'enemy_pos':[{'x':0,"y":7,'time':1000,'tanks_list':[0,3,2,1,1,2,3],'sleep':3000,'wave_start':False},
                      {'x':7,"y":7,'time':2000,'tanks_list':[2,3,1,1,0,0],'sleep':10000,'wave_start':False},
-                     {'x':1,"y":7,'time':3000,'tanks_list':[],'sleep':5000,'wave_start':False}]
+                     {'x':1,"y":7,'time':3000,'tanks_list':[],'sleep':5000,'wave_start':False}],
+        'name':'level_test'
 
 }
 levels_list.append(level1)
--- model_menu_levels.py
+++ model_menu_levels.py
@@ -4,3 +4,18 @@ pygame.init()
 mananger=pygame_gui.UIManager((1000,1000),'gui_levels.json')
 level1=pygame.rect.Rect([50,50,210,210])
 button_level1=pygame_gui.elements.UIButton(level1,'',mananger,object_id='#level_1')
+level_name=1
+offsets=50
+button_colx=4
+button_coly=3
+button_size=210
+offsets_buttons=(1000-button_size*button_colx-offsets*2)/(button_colx-1)
+text_sizey=80
+for line_y in range(button_coly):
+    for line_x in range(button_colx):
+        level_rect_button = pygame.rect.Rect([offsets+button_size*line_x+offsets_buttons*line_x, 50+button_size*line_y+100*line_y, button_size, button_size])
+        button_level=pygame_gui.elements.UIButton(level_rect_button,'',mananger,object_id=pygame_gui.core.ObjectID('#level_'+str(level_name),'@level_buttons'))
+        level_rect_label=pygame.rect.Rect([offsets+button_size*line_x+offsets_buttons*line_x,level_rect_button.bottom,button_size,text_sizey])
+        pygame_gui.elements.UILabel(level_rect_label,'test',mananger,object_id='@level_labels')
+        level_name+=1
+
